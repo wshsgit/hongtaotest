b49de6a74680723884d20ec99d188a50086cbeac
wss <18510096886@163.com>
2018-08-29 17:12:58 +0800

    修改后的一次提交和推送

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("开始");
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("ha"+i);
+        }
     }
 }
